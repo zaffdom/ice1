b64260f6d89bab82906035d1dbad850ad99a2665
ronak1 <ronak.sheth@sheridancollege.ca>
2019-02-01 10:02:48 -0500

    First Commit

---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
+
+/**
+ *
+ * @author Ronak
+ */
+public class Card {
+
+   private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+   
+   
+    
+}
+
+
---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -0,0 +1,14 @@
+
+package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
+
+/**
+ *
+ * @author Ronak
+ */
+public class CardTrick {
+    public static void main(String[] args)
+    {
+       
+    }
+    
+}
