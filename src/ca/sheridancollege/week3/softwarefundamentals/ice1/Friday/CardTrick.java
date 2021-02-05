769302d112562d750c92fff62e507f8d9a8e2456
Domenic Zaffino <78223933+zaffdom@users.noreply.github.com>
2021-02-05 12:55:42 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,14 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card C = new Card();
+       String c2;
+       int c3;
+       c.setSuit("Spades");
+       c2=(c.getSuit());
+       System.out.println("Suit: "+c2);
+       c.setValue(6);
+       c3=(c.getValue());
+       System.out.println("value: "+c3);
     }
     
 }
