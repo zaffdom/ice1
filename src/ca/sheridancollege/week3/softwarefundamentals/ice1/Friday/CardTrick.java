26e8d392b0b021c99a67656cc5a878e3d7e48277
zaffdom <zaffidom@sheridancollege.ca>
2021-02-05 12:50:41 -0500

    added object "C" of type Card and changed comment headers

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,8 +2,8 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
- * @author Ronak
+ *Student #:991608983
+ * @author Domenic Zaffino
  */
 public class CardTrick {
     public static void main(String[] args)
