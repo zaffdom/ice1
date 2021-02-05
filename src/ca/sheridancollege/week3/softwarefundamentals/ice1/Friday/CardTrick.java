132e8d6359b86018b4fc7ff64fc9f769e6844ab5
zaffdom <zaffidom@sheridancollege.ca>
2021-02-05 12:52:40 -0500

    added object "C" of type Card and changed comment headers

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,7 +8,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card C = new Card();
     }
     
 }
